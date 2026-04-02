1878b7254732579916094bb2362ef7187c4aa861
joaotoledo18 <joaovtsilva5a@gmail.com>
2026-04-02 19:39:37 -0300

    config api

--- BackEnd/src/api.js
+++ BackEnd/src/api.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const api = express()
-const cors = require('cors')
+const express = require("express");
+const api = express();
+const cors = require("cors");
+const routers = require("./routers/routers");
 
-api.use(cors())
-api.use(express.urlencoded({extended: false}))
-api.use(express.json())
+api.use(cors());
+api.use(express.urlencoded({ extended: false }));
+api.use(express.json());
+api.use("/", routers);
 
-module.exports = api
+module.exports = api;

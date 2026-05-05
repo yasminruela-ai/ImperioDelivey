4030581c44539f62e8adf27fb4b902136be9f7d5
joaotoledo18 <joaovtsilva5a@gmail.com>
2026-05-04 21:43:32 -0300

    rodando local

--- BackEnd/src/config/firebase.js
+++ BackEnd/src/config/firebase.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../env") });
+require("dotenv").config();
 
 const adminFirebase = require("firebase-admin");
 
